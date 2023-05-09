426db4324f3afbd8f7332becccd629983b6d1dfd
Anton Borisov <antobis1983@gmail.com>
2023-05-10 00:49:34 +0300

    rename gendiff to gendiff_package

---
+++ gendiff_package/scripts/gendiff.py
@@ -0,0 +1,14 @@
+#export PYTHOPATH = '$PYTHOPATH:python-project-50/gendiff/ '
+import sys
+import os
+# from gendiff import gendiff_args
+
+
+# def main():
+#     gendiff_args()
+#
+#
+# if __name__ == '__main__':
+#     main()
+print(sys.path)
+print(os.path.dirname(__file__))

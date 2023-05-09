1909fe7f11a102b372b098b1247efada069eec13
Anton Borisov <antobis1983@gmail.com>
2023-05-10 00:59:58 +0300

    modified gendiff_package, pyproject.toml

--- gendiff_package/scripts/gendiff.py
+++ gendiff_package/scripts/gendiff.py
@@ -1,14 +1,10 @@
-#export PYTHOPATH = '$PYTHOPATH:python-project-50/gendiff/ '
-import sys
-import os
-# from gendiff import gendiff_args
+from gendiff_package.gendiff_args import gendiff_args
 
 
-# def main():
-#     gendiff_args()
-#
-#
-# if __name__ == '__main__':
-#     main()
-print(sys.path)
-print(os.path.dirname(__file__))
+def main():
+    gendiff_args()
+
+
+if __name__ == '__main__':
+    main()
+

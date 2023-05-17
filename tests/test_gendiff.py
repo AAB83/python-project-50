efdb9edfb290515409e6fada81244b827073412d
Anton Borisov <antobis1983@gmail.com>
2023-05-17 19:34:50 +0300

    change test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,13 +4,13 @@ from gendiff import generate_diff
 
 @pytest.fixture()
 def data():
-    file_path1 = 'fixtures/file1.json'
-    file_path2 = 'fixtures/file2.json'
+    file_path1 = 'tests/fixtures/file1.json'
+    file_path2 = 'tests/fixtures/file2.json'
     return file_path1, file_path2
 
 
 def test_generate_diff(data):
     file_path1, file_path2 = data
-    result = open('fixtures/result_1.txt').read()
+    result = open('tests/fixtures/result_1.txt').read()
     diff = generate_diff(file_path1, file_path2)
     assert diff == result

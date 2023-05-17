f212aff92a30a22d72d9f7926d3189ccb620fa6b
Anton Borisov <antobis1983@gmail.com>
2023-05-17 11:53:48 +0300

    change gendiff.py, step 3

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -15,12 +15,31 @@ def gendiff_args():
 
 
 def generate_diff(file_path1, file_path2):
+    diff = []
     file_1 = json.load(open(file_path1))
     file_2 = json.load(open(file_path2))
-    print(f'{file_1}, {type(file_1)=}')
-    print(f'{file_2}, {type(file_2)=}')
+
+    key_set_file1 = set(file_1.keys())
+    key_set_file2 = set(file_2.keys())
+
+    key_remote = key_set_file1 - key_set_file2
+    key_add = key_set_file2 - key_set_file1
+    key_same = key_set_file1 & key_set_file2
+
+    diff += list(map(lambda item: f'  - {item}:{file_1[item]}', key_remote))
+    diff += list(map(lambda item: f'  + {item}:{file_2[item]}', key_add))
+    for item in key_same:
+        if file_1[item] == file_2[item]:
+            diff.append(f'    {item}:{file_1[item]}')
+        else:
+            diff.append(f'  - {item}:{file_1[item]}')
+            diff.append(f'  + {item}:{file_2[item]}')
+    diff = ['{'] + diff
+    diff.append('}')
+    return '\n'.join(diff)
 
 
 def gendiff():
     file_path1, file_path2 = gendiff_args()
-    generate_diff(file_path1, file_path2)
+    diff = generate_diff(file_path1, file_path2)
+    print(diff)
